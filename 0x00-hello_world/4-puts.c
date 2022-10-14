283b5e94563bd0fbdc90b899aee8919b1658895f
“Audrey_L” <adamswinifred@gmail.com>
2022-10-13 19:46:08 -0700

    Multilingual

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 /**
  *  *main- Serves as the entry point for the program
  *   *
- *    *Return: By default returns zero if no erros otherwise returns non-zero value.
+ *    *Return: Always 0 (Success)
  *     */
 int main(void)
 {
-			puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+		puts ("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
